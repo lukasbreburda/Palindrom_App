be771d69de2af70820b7a350a7b4550beaf20725
Lukáš Breburda <breburda.dolakova@gmail.com>
2016-12-06 10:44:31 +0100

    Palindrom_App

---
+++ Page1.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Palindrom_App
+{
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        public void over(object sender, EventArgs e)
+        {
+            if (overeni(text.Text) == true)
+            {
+                vys.Text = "Jedná se o palindrom";
+            }
+            else
+            {
+                vys.Text = "Nejedná se o palindrom";
+                
+            }
+        }
+        public static bool overeni(string retezec)
+        {
+            char[] array = retezec.ToCharArray();
+            Array.Reverse(array);
+            string backwards = new string(array);
+            if (retezec == backwards)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
